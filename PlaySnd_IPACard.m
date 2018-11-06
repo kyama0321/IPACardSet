8d6b8577cc6de9d22589043e913b2d4b6070ca6e
YamaKatsu <yamamoto.katsuhiko@g.wakayama-u.jp>
2018-11-06 21:24:32 +0900

    Add select menu for game types

--- PlaySnd_IPACard.m
+++ PlaySnd_IPACard.m
@@ -2,56 +2,90 @@
 %
 % PlaySnd_IPACard
 %
-% Yamamoto, K. 
+% YamaKatsu (@kyama0321)
 % Created: 2 Nov. 2018
+% Modified: 6 Nov. 2018
+%
+% Example: 
+% > Select game type(s) -> 1 (vowels only)
+% > Select game type(s) -> 1 2 3 (vowels and consonants)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%
-% Set your enviroment
-dirRoot = [getenv('HOME') '/Desktop/TmpFiles/IPACardSet/'];
+%% Set your enviroment
+dirRoot = [getenv('HOME') '/Desktop/IPACardSet/Code/'];
 dirSnd  = [dirRoot 'Sounds/'];
-%%%%%%%%%%%%%%%%%%%%%
 
-% Set game levels (under construction)
-swLevel = 0;
-if swLevel == 1 % Vowels
-    numIPACard = 71:98;
-elseif swLevel == 2     % Consonants (pulmonic)
-    numIPACard = 1:59;
-elseif swLevel == 3     % Consonants (non-pulmonic)
-    numIPACard = [60:70,109:111];
-elseif swLevel == 4     % Other symbols
-    numIPACard = 99:108;
-else                    % All
-    numIPACard = 1:111;
-end
-    
 % Set repeat times
 numRep = 3;
 
-% Load a list of the IPACard
-nameListSnd = 'IPACard_ListSnd.csv';
+% Load a downloard list of the IPA Card Set
+nameListSnd = 'ListSnd_IPACard.csv';
 formatSpec = '%s';
-fileID = fopen(nameListSnd);
+fileID = fopen([dirRoot nameListSnd]);
 strListSnd = textscan(fileID,formatSpec);
 fclose(fileID);
 
-% Randomizing with a seed number
-seed = 20181102;
-rng(seed);
-numPlaySnd = randperm(max(numIPACard));
+%% Menu
+% Clear command window
+clc
+
+% Set game types
+strMsg = [...
+        '========================================\n',...
+        ' IPA Card "Karuta"\n',...
+        ' Author: YamaKatsu (@kyama0321)\n',...
+        ' Date: 6 Nov. 2018\n',...
+        '========================================\n',...
+        '   1. Vowels: No.71 ~ No.98 \n',...
+        '   2. Consonants (pulmonic): No.1 ~ No.59\n',...
+        '   3. Consonants (non-pulmonic): No.60 ~ No.70, No.109 ~ No.111\n',...
+        '   4. Other symbols: No.99 ~ No.108\n\n'];
+fprintf(strMsg);
+
+% Check inputs
+numSwType = []; % Initialize
+while isempty(numSwType) == 1
+    prompt = 'Select game type(s) -> ';
+    strSwType = input(prompt,'s');
+    numSwType = str2num(strSwType);
+end
+
+% Stack card IDs
+numIPACard = []; % Initialize
+for i = numSwType
+    if numSwType(i) == 1 % Vowels: 71~98
+        numIPACard = horzcat(numIPACard,[71:98]);
+    elseif numSwType(i) == 2     % Consonants (pulmonic): 1~59
+        numIPACard = horzcat(numIPACard,[1:59]);
+    elseif numSwType(i) == 3     % Consonants (non-pulmonic): 60~70, 109~111
+        numIPACard = horzcat(numIPACard,[60:70,109:111]);
+    elseif numSwType(i) == 4     % Other symbols: 99~108
+        numIPACard = horzcat(numIPACard,[99:108]);
+    else                    % All: 1~111;
+        error('Set game type: 1~4');
+    end
+end
+
+
+% Randomizing
+%seed = 20181102;  %with a seed number (option)
+%rng(seed);
+numPlaySnd = randperm(length(numIPACard));
 
 
 %% Start IPA Card "Karuta"
 % Waiting
-swRep = 'Start IPA Card "Karuta": RETURN/ENTER';
+swRep = '\n\nStart IPA Card "Karuta": RETURN/ENTER';
 input(swRep);
 
-for i = numPlaySnd
+for j = numPlaySnd
+    
+    % ID
+    idxIPACard = numIPACard(j);
     
     % Extract a name of PlaySnd
-    strIDPlaySnd = strListSnd{1}{i};
+    strIDPlaySnd = strListSnd{1}{idxIPACard};
+    %htmlIDPlaySnd = ['<a href = "' strIDPlaySnd '">' strIDPlaySnd '</a>'];
     
     if ~contains(strIDPlaySnd, 'File%3A') == 1  % https://en.wikipedia or https://commons.wikimedia.org/wiki/
         startStr = 'File:';
@@ -62,12 +96,14 @@ for i = numPlaySnd
     end
     
     % Load sound data
-    DirPlaySnd = [dirSnd num2str(i, '%03d') '_' strNamePlaySnd];
+    DirPlaySnd = [dirSnd num2str(idxIPACard, '%03d') '_' strNamePlaySnd];
     [playSnd, fs] = audioread(DirPlaySnd);
     
-    disp('====================================================')
-    disp(['Playing: ' num2str(i, '%03d') '_' strNamePlaySnd]);
-    disp('====================================================')
+    disp(' ');
+    disp('========================================');
+    disp(['Playing: ' num2str(idxIPACard, '%03d') '_' strNamePlaySnd]);
+    %disp(htmlIDPlaySnd);
+    disp('========================================');
     
     % Calibrate the signal level to -26 in the digital level (RMS)
     rmsdBSnd = 20*log10(sqrt(mean(playSnd.^2)));
@@ -91,3 +127,7 @@ for i = numPlaySnd
     end
     
 end
+
+disp('====================================================')
+disp('Finish!!!');
+disp('====================================================')
